85760729057195edb60cfcdbf8550977b3999e54
Juan Ignacio <Juan Ignacio@192.168.0.208>
2017-11-09 04:06:05 -0300

    Finalizado metodo guardarTitular

--- TrabajoPractico/src/Persistencia/GestorTitular.java
+++ TrabajoPractico/src/Persistencia/GestorTitular.java
@@ -3,6 +3,7 @@ package Persistencia;
 import DAO.DAOTitular;
 import DTO.DTOTitular;
 import Entidades.Titular;
+import javax.swing.JOptionPane;
 
 public class GestorTitular {
     DAOTitular dao;
@@ -11,28 +12,11 @@ public class GestorTitular {
         
     }
     
-    public boolean guardarTitular(DTOTitular titular){
+    public boolean guardarTitular(Titular titular){
         dao = new DAOTitular();
-        Titular tit = new Titular();
-        tit.apellido = titular.getApellido();
-        tit.calle = titular.getCalle();
-        tit.departamento = titular.getDepartamento();
-        tit.dni = titular.getDni();
-        tit.donante = titular.getDonante();
-        tit.factor = titular.getFactor();
-        tit.fechaAlta = titular.getFechaAlta();
-        tit.fechaNacimiento = titular.getFechaNacimiento();
-        tit.grupo = titular.getGrupo();
-        tit.idTitular = -1;
-        tit.localidad = titular.getLocalidad();
-        tit.nombre = titular.getNombre();
-        tit.numero = titular.getNumero();
-        tit.pais = titular.getPais();
-        tit.piso = titular.getPiso();
-        tit.provincia = titular.getProvincia();
-        tit.tipoDni = titular.getTipoDni();
-        if (!existeTitular(tit) && esContribuyente(titular.getDni())){
-            return dao.insertTitular(tit);
+               
+        if (!existeTitular(titular) && esContribuyente(titular.dni)){
+            return dao.insertTitular(titular);
         }
         else
             return false;
