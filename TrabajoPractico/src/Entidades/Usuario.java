4d029de0e5da7cbfada4e2f5efb69327eb68bc8a
Juan Ignacio <Juan Ignacio@192.168.0.208>
2017-11-09 06:00:37 -0300

    Reacomodada clase usuario

---
+++ TrabajoPractico/src/Entidades/Usuario.java
@@ -0,0 +1,78 @@
+package Entidades;
+
+public class Usuario {
+    
+    int id, dni;
+    String nombre, apellido, usuario, contrasenia, cargo;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, Integer dni, String nombre, String apellido, String usuario, String contrasenia, String cargo) {
+        this.id = id;
+        this.dni = dni;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+        this.cargo = cargo;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getDni() {
+        return dni;
+    }
+
+    public void setDni(Integer dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+    
+    
+}
